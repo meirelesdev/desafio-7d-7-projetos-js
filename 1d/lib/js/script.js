0b0641de646a97c8b96f0a33d9c4fd9b48692bf4
Daniel Meireles do Nascimento <daniel@inovadora.com.br>
2021-07-19 09:51:08 -0300

    projeto finalizado

---
+++ 1d/lib/js/script.js
@@ -0,0 +1,32 @@
+document.body.addEventListener('keyup', event => {
+    playSound(event.code.toLowerCase())
+})
+document.querySelector('.composer button').addEventListener('click', ()=>{
+    let song = document.querySelector('#input').value
+    if (song !== '') playComposition(song.split(''))
+})
+const playSound = sound => {
+    let audioElement = document.querySelector(`#s_${sound}`)
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`)
+    if (keyElement) activeButton(keyElement)
+    if (audioElement) playAudio(audioElement)
+}
+const activeButton = (keyElement) => {
+    keyElement.classList.add('active')
+    setTimeout(()=>{
+        keyElement.classList.remove('active')
+    }, 300)
+}
+const playAudio = audioElement => {
+    audioElement.currentTime = 0
+    audioElement.play()
+}
+const playComposition = songArray => {
+    let wait = 0
+    for(let songItem of songArray) {
+        setTimeout(()=>{
+            playSound(`key${songItem}`)
+        }, wait)
+        wait += 250
+    }
+}
