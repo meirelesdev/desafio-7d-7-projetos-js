cb01ac4d6a0942b3a1d7221cb4d27c7bf1c97376
Daniel Meireles do Nascimento <daniel@inovadora.com.br>
2021-07-23 09:15:08 -0300

    finalizando ajustes

--- 4d/assets/js/script.js
+++ 4d/assets/js/script.js
@@ -8,9 +8,11 @@ let typesOfVictory = [
     'a1,a2,a3',
     'b1,b2,b3',
     'c1,c2,c3',
+
     'a1,b1,c1',
     'a2,b2,c2',
     'a3,b3,c3',
+
     'a1,b2,c3',
     'a3,b2,c1',
 ]
@@ -37,6 +39,7 @@ const renderBoard = () =>{
     }
     checkGame()
 }
+
 const renderInfo = () => {
     document.querySelector('.vez').innerHTML = turn
     document.querySelector('.resultado').innerHTML = warning
